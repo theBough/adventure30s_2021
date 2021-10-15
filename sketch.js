91daea4bcbf26d3edbd02b08c9f1220bdbe45af6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-15 11:42:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
 let p;
+let w =[];
 function setup() {
   createCanvas(400, 400);
   p = new Player(200,200,10,10,"red");
+  w.push(new Wall(0,0,400,10,"green"));
+  w.push(new Wall(0,390,400,10,"green"))
 }
 
 function draw() {
-  background(225);
+  background(0);
   p.display();
   p.update();
+  for(i=0 ; i < w.length ; i++){
+    w[i].display();  
+  }//end loop
+  
   
 }
