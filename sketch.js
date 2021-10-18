5670a7bd07114e7d0b95c4f72a6f1af947b4cc5a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-18 11:22:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,43 @@
 let p;
-let w =[];
+let w = [];
+let room = 1;
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,10,10,"red");
-  w.push(new Wall(0,0,400,10,"green"));
-  w.push(new Wall(0,390,400,10,"green"))
+  p = new Player(200, 200, 10, 10, "red");
+  roomOne();
 }
-
 function draw() {
   background(0);
   p.display();
   p.update();
-  for(i=0 ; i < w.length ; i++){
-    w[i].display();  
-  }//end loop
-  
-  
+  for (i = 0; i < w.length; i++) {
+    w[i].display();
+  } //end loop
+  checkChangeScreen();
 }
+function checkChangeScreen(){
+  if(p.x + p.w > width){
+    p.x = 0;
+    whereTo("r");
+  }
+}
+
+function whereTo(whatDirection) {
+  w = [];
+  if (room == 1) {
+    switch (whatDirection) {
+      case "u":
+        
+        break;
+      case "d":
+        
+        break;
+      case "l":
+        
+        break;
+      case "r":
+        roomTwo();
+        break;
+    } //end switch
+  } //end if
+} //end whereTo
