751315d25a67ea59fc3c2449dd7ea3884e1453d1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-18 11:23:31 -0500

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,11 @@
+function roomOne(){
+  w = [];
+  w.push(new Wall(0,0,400,10,"green"));
+  w.push(new Wall(0,390,400,10,"green"));
+}
+
+function roomTwo(){
+  w = [];
+  room = 2;
+  w.push( new Wall (200,200,200,10,"pink"));
+}
