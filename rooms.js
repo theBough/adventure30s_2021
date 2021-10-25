9a348de6b15ab4959f1147f196b4f964de53be81
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-25 10:53:33 -0500

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,11 +1,17 @@
 function roomOne(){
   w = [];
+  room =1
   w.push(new Wall(0,0,400,10,"green"));
   w.push(new Wall(0,390,400,10,"green"));
 }
 
 function roomTwo(){
-  w = [];
+  w= [];
   room = 2;
   w.push( new Wall (200,200,200,10,"pink"));
+}
+function roomThree(){
+  w = [];
+  room = 3;
+  w.push( new Wall (100,200,200,10,"yellow"));
 }
