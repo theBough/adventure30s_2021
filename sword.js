3fe943a882e331b1bb8f5a427cb2eed44927d719
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-11-02 11:42:26 -0500

    Update sword.js

--- sword.js
+++ sword.js
@@ -1,14 +1,33 @@
-function Sword(x,y,w,h){
+let haveSword = false;
+function Sword(x, y, w, h) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.show = true;
-  
-  this.display = function(){
-    if(this.show){
+
+  this.display = function () {
+    if (this.show) {
       fill(200);
       rect(this.x, this.y, this.w, this.h);
+      swordCollision();
+    } //end if
+  }; //end display
+
+  function swordCollision() {
+    if (
+      p.y < s.y &&
+      p.y + p.h > s.y + s.h &&
+      p.x + p.w > s.x &&
+      p.x + p.w < s.x + s.w &&
+      !haveSword &&
+      room == 1
+    ) {
+      haveSword = true;
+    } //end if
+    if (haveSword) {
+      s.x = p.x + p.w;
+      s.y = p.y + p.h / 2;
     }//end if
-  }//end display
-}//end Sword
+  }//end SwordCollision
+} //end Sword
